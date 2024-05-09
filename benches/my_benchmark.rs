8839b193087882e1ecc34a3bf1a4d3e8452ad483
Malaparte4 <113225642+Malaparte4@users.noreply.github.com>
2024-05-09 10:00:18 -0500

    remove old benchmark method, remove comment

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,22 +1,11 @@
 use chrono::Utc;
 use criterion::async_executor::FuturesExecutor;
 use criterion::{criterion_group, criterion_main, Criterion};
-use lock_free_logging::*;
+use lib::example_types::*;
+use lib::logger::*;
 
-// Bench formatting struct as LogMsg
-async fn to_log_message_bench() {
-    let _data = ExampleTradeStream {
-        symbol: "BTCUSDT".to_string(),
-        side: "buy".to_string(),
-        qty: 2,
-        price: 46030.50,
-        timestamp: Utc::now().timestamp_millis().to_string(),
-    };
-    usage_example::<ExampleTradeStream>(_data.into())
-        .await
-        .unwrap();
-}
-
+//NOTE: Bench prints
+//
 // Bench Logging the LogMsg with enum defined in logging function
 async fn log_bench() {
     let _ = async_logging_thread().await;
@@ -29,16 +18,13 @@ async fn owned_log_msg_passed_to_logger_bench() {
         side: "buy".to_string(),
         qty: 1,
         fill_price: 46030.50,
-        timestamp: Utc::now().timestamp_millis().to_string(),
+        timestamp: Utc::now().timestamp_millis(),
     });
-    // let message = Arc::new(LogMsg::Event(data));
-    let _ = async_logging_thread_with_message::<OwnedLogMsg>(data.into()).await;
+
+    let _ = async_logger::<OwnedLogMsg>(data.into()).await;
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("struct to LogMsg", |b| {
-        b.to_async(FuturesExecutor).iter(|| to_log_message_bench());
-    });
     c.bench_function("Message formatted in log function", |b| {
         b.to_async(FuturesExecutor).iter(|| log_bench());
     });
@@ -49,37 +35,3 @@ fn criterion_benchmark(c: &mut Criterion) {
 }
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
-
-//
-// fn bench_function(c: &mut Criterion) {
-//     let rt = Runtime::new().unwrap();
-//     c.bench_function("async_logging_bench", |b| {
-//         b.to_async(&rt).iter(|| async {
-//             let _data = ExampleTradeStream {
-//                 symbol: "BTCUSDT".to_string(),
-//                 side: "buy".to_string(),
-//                 qty: 1,
-//                 price: 46030.50,
-//                 timestamp: Utc::now().timestamp_millis().to_string(),
-//             };
-//             let _ = usage_example::<ExampleTradeStream>(_data.into()).await;
-//
-//             let data = OwnedEventType::MarketTradeData {
-//                 symbol: ("BTCUSDT".to_string()),
-//                 side: ("BUY".to_string()),
-//                 qty: (1000),
-//                 fill_price: (29000.69),
-//                 timestamp: (Utc::now().timestamp_millis().to_string()),
-//             };
-//
-//             //NOTE Example directly to log_thread
-//             let message = Arc::new(OwnedLogMsg::Event(data));
-//             let test = async_logging_thread_with_message::<OwnedLogMsg>(message).await;
-//
-//             // async_logging_thread_with_message(Arc::new(_data)).await;
-//         });
-//     });
-// }
-// fn test() -> Result<()> {
-//
-// }
