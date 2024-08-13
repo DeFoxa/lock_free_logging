c9f7040bb831da47fd658fc4fe34550f77f01fe8
Malaparte4 <113225642+Malaparte4@users.noreply.github.com>
2024-08-13 11:18:04 -0500

    simple enum logger for bench comparison

---
+++ src/enum_logger.rs
@@ -0,0 +1,18 @@
+use crate::example_types::ExampleOB;
+use lockfree::channel::spsc::{create, Receiver, Sender};
+use std::thread;
+
+async fn enum_logger(event: ExampleOB) {
+    let (mut tx, mut rx) = create::<ExampleOB>();
+
+    let guard = thread::spawn(move || {
+        let core_ids = core_affinity::get_core_ids().unwrap();
+        core_affinity::set_for_current(*core_ids.last().unwrap());
+
+        match (rx.recv()) {
+            Ok(msg) => println!("ob update received"),
+            Err(e) => panic!("receive error "),
+        }
+    });
+    tx.send(event);
+}
